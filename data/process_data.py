93b5cb8d67e7252e85804eb5077639941ae711ed
Victor Lambert <victorl@blend.com>
2018-11-25 16:31:04 -0500

    finalize the process_data script

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,45 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    """loads the specified message and category data"""
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return pd.merge(messages, categories, on='id')
 
 def clean_data(df):
-    pass
+    """Cleans the data:
+        - drops duplicates
+        - removes messages missing classes
+        - cleans up the categories column
+    """
+    # expand the categories column
+    categories = df.categories.str.split(';', expand=True)
+    row = categories[:1]
 
+    # get the category names
+    category_colnames = row.applymap(lambda s: s[:-2]).iloc[0, :].tolist()
+    categories.columns = category_colnames
+
+    # get only the last value in each value as an integer
+    categories = categories.applymap(lambda s: int(s[-1]))
+
+    # add the categories back to the original df
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # clean up the final data
+    df.drop_duplicates(subset='message', inplace=True)
+    df.dropna(subset=category_colnames, inplace=True)
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
-
+    """Saves the resulting data to a sqlite db"""
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('labeled_messages', engine, index=False, if_exists='replace')
+    engine.dispose()
 
 def main():
     if len(sys.argv) == 4:
@@ -24,12 +52,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
